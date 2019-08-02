2562ed2987751578da9d335874ea6cd0eb036d25
Vegardvv <53624513+Vegardvv@users.noreply.github.com>
2019-08-02 21:58:18 +0200

    Update number guesser.py

--- number guesser.py
+++ number guesser.py
@@ -3,28 +3,45 @@
 from random import randint
 
 print("Hello. Guess the right number between 1 and 10 to win!")
+while True:
+	
+	# It picks a number between 1-10
+	Win_number = randint(1,10)
 
-# It picks a number between 1-10
-Win_number = randint(1,10)
+	# We define guess as an integer. The value will be 'None'
+	guess = int()
 
-# We define guess as an integer. The value will be 'None'
-guess = int()
+	# We don't want the loop to start before we have an input.
+	while guess != Win_number:
+		try:
+			guess = int(input("Guess? > "))
+		except KeyboardInterrupt:
+			print("\nCtrl + C detected! Exiting...")
+			exit()
+		except:
+			print("Ooops! You must type in a number")
+			continue
 
-# We don't want the loop to start before we have an input.
-while guess != Win_number:
-	try:
-		guess = int(input("Guess? > "))
-	except KeyboardInterrupt:
-		print("\nCtrl + C detected! Exiting...")
-		break
-	except:
-		print("Ooops! You must type in a number")
-		continue
+		if guess > Win_number:
+			print("Try a lower number")
+		elif guess < Win_number:
+			print("Try a higher number")
+		else:
+			print("That was right! you win! :)\nWould you like to play again? Y/N")
+	while True:
+		try:
 
-	if guess > Win_number:
-		print("Try a lower number")
-	elif guess < Win_number:
-		print("Try a higher number")
-	else:
-		print("That was right! you win! :)")
-		break # Instead of 'exit()', you can break the while loop by using 'break'
+			
+			Play_again = input("Play again? y/n:")
+		except KeyboardInterrupt:
+			print("\nCtrl + C detected! Exiting...")
+			exit()
+		if Play_again == "y":
+			print("Guess the number again!")
+			break
+		
+		elif Play_again == "n":
+			print("Thanks for playing! :)")
+			exit()
+		else:
+			print("Invalid answer")
